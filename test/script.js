abd20a751760a52d2e3ea7b7f5b14b5ddbc200ab
emiramirul <73177278+emiramirul@users.noreply.github.com>
2024-06-18 19:18:11 +0800

    Add files via upload

---
+++ test/script.js
@@ -0,0 +1,36 @@
+let currentImageIndex = 0;
+const images = ["nail-polish.jpg", "nail-polish2.jpg"];
+const productImg = document.getElementById("product-img");
+
+function showImage(index) {
+    productImg.src = images[index];
+}
+
+function nextImage() {
+    currentImageIndex = (currentImageIndex + 1) % images.length;
+    showImage(currentImageIndex);
+}
+
+function prevImage() {
+    currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
+    showImage(currentImageIndex);
+}
+
+// Modal
+const modal = document.getElementById("modal");
+const btn = document.getElementById("buy-now");
+const span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
